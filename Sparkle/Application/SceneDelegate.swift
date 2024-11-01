1271f637f31fa53a0b421f6cf8d74d13b613aad6
choiminkyung <choialsrud2002@gmail.com>
2024-11-01 17:07:21 +0900

    [feat/#1] 온보딩 UI 구현

--- Sparkle/Application/SceneDelegate.swift
+++ Sparkle/Application/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let vc = SparkleTabBarController()
+        let vc = OnboardingViewController()
         window.rootViewController = vc
         
         window.makeKeyAndVisible()
