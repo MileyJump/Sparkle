52b2bbbb97461ead5a549a018eb48dad30a187bf
choiminkyung <choialsrud2002@gmail.com>
2024-11-02 16:58:57 +0900

    [feat/#4] Custom Workspace NavigationBar  구현

---
+++ Sparkle/Global/CustomNavigationBar.swift
@@ -0,0 +1,8 @@
+//
+//  CustomNavigationBar.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/1/24.
+//
+
+import Foundation
---
+++ Sparkle/Global/UIViewController +.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController +.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/2/24.
+//
+
+import Foundation
