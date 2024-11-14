245870f69d20dc1142b8b811d936bf23b80bd4ad
choiminkyung <choialsrud2002@gmail.com>
2024-11-14 16:43:10 +0900

    [feat/#6] user API target enum 작성

---
+++ Sparkle/Network/UserAPI.swift
@@ -0,0 +1,22 @@
+//
+//  UserAPI.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/14/24.
+//
+
+import Foundation
+
+enum UserAPI {
+    case join
+    case emailValidation
+    case login
+    case kakaoLogin
+    case appleLogin
+    case logout
+    case deviceToken
+    case profileCheck
+    case profileModification
+    case profileImageModification
+    case userProfileCheck
+}
