4b23cae3e5b6e65091c20d217534d965e9cf1290
choiminkyung <choialsrud2002@gmail.com>
2024-11-15 23:16:58 +0900

    [feat/#6] workspace API target enum 작성

---
+++ Sparkle/Network/WorkspaceAPI.swift
@@ -0,0 +1,21 @@
+//
+//  WorkspaceAPI.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/15/24.
+//
+
+import Foundation
+
+enum WorkspaceAPI {
+    case workspacesListCheck
+    case createWorkspace
+    case workspaceInformationCheck
+    case workspaceEdit
+    case workspaceDelete
+    case workspaceMembersInvite
+    case workspaceSpecificMemberCheck
+    case workspaceSearch
+    case changeWorkspaceAdministrator
+    case exitWorkspace
+}
