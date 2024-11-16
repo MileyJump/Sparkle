f105359583f6cecbe59a7d065d8942ccc9b9f027
choiminkyung <choialsrud2002@gmail.com>
2024-11-16 23:23:19 +0900

    [feat/#6] channel  API target enum 작성

---
+++ Sparkle/Network/ChannelAPI.swift
@@ -0,0 +1,23 @@
+//
+//  ChannelAPI.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/16/24.
+//
+
+import Foundation
+
+enum ChannelAPI {
+    case mychannelCheck
+    case channelListCheck
+    case createChannel
+    case specificChannelCheck
+    case channelsEdit
+    case channelsDelete
+    case channelChatHistoryList
+    case sendChannelChat
+    case numberOfUnreadChannelChats
+    case channelMembers
+    case changeChannelManager
+    case leaveChannel
+}
