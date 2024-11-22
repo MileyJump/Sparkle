d90d5bd8747bdf866399a4ec50060658d5976d02
choiminkyung <choialsrud2002@gmail.com>
2024-11-22 21:41:03 +0900

    [feat/#5] HomeDefaultView UI 레이아웃

---
+++ Sparkle/Presentation/WorkSpace/Home/ViewController/HomeDefaultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeDefaultViewController.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/22/24.
+//
+
+import UIKit
+
+final class HomeDefaultViewController: BaseViewController<HomeDefaultView> {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
--- Sparkle/SceneDelegate.swift
+++ Sparkle/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
         
-        let vc = workspaceInitialViewController()
+        let vc = HomeDefaultViewController()
         let nav = UINavigationController(rootViewController: vc)
         window.rootViewController = nav
         
