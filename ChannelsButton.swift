0573f778a2a63aed023e54dc21cd734db8fb2232
choiminkyung <choialsrud2002@gmail.com>
2024-11-23 23:11:33 +0900

    [feat/#5] HomeDefaultView UI 레이아웃 수정

--- ChannelsButton.swift
+++ ChannelsButton.swift
@@ -7,39 +7,47 @@
 
 import UIKit
 
+import SnapKit
+
 final class ChannelsButton: UIButton {
     
     init(title: String, image: UIImage?, backgroundColor: UIColor, tintColor: UIColor, font: UIFont) {
         super.init(frame: .zero)
         
-        var config = UIButton.Configuration.plain()
-        config.image = image
-        config.imagePadding = 8
-        config.imagePlacement = .trailing
-        config.baseBackgroundColor = backgroundColor
-        config.baseForegroundColor = tintColor
-        config.cornerStyle = .medium
-        
-    
-        if let image = image {
-            let resizedImage = image.withConfiguration(UIImage.SymbolConfiguration(pointSize: 13)) 
-            config.image = resizedImage
-        }
-        
-        var attributedTitle = AttributedString(title)
-        attributedTitle.font = font
-        attributedTitle.foregroundColor = tintColor
-        config.attributedTitle = attributedTitle
-        
-        self.configuration = config
-        
+        // 버튼 기본 스타일 설정
+        self.backgroundColor = backgroundColor
+        self.tintColor = tintColor
         self.layer.cornerRadius = 8
         self.clipsToBounds = true
+        
+        let titleLabel = UILabel()
+        titleLabel.text = title
+        titleLabel.font = font
+        titleLabel.textColor = tintColor
+        self.addSubview(titleLabel)
+        
+        
+        let imageView = UIImageView()
+        if let image = image {
+            imageView.image = image.withConfiguration(UIImage.SymbolConfiguration(pointSize: 13))
+        }
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        self.addSubview(imageView)
+        
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-16)
+            make.centerY.equalToSuperview()
+        }
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
-
