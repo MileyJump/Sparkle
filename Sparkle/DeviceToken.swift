744bfa06c43e727cc26e0634d052ea4f3faac993
choiminkyung <choialsrud2002@gmail.com>
2024-11-27 23:41:19 +0900

    [feat/#5] 수정

---
+++ Sparkle/DeviceToken.swift
@@ -0,0 +1,12 @@
+//
+//  DeviceToken.swift
+//  Sparkle
+//
+//  Created by 최민경 on 11/25/24.
+//
+
+import Foundation
+
+enum DeviceToken {
+    static let deviceToken = "emo2pCV-R0vEnbJJpS-tEB:APA91bF1BE1liYItzinoKXi4KrD5aQ37KJPrRxx037wxzBxGQENKxELmp8bbZ0kXDQqIQvgPXNOWBxJ67atZSv4W6KDYaBvPJFAIZybrZtdDUCJt5vjxzzzKNzSqxU8sM0XggZdxHzri"
+}
