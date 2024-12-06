cb697528ac175719c36d4fe3477989ab68616b3a
choiminkyung <choialsrud2002@gmail.com>
2024-12-06 15:08:29 +0900

    [feat/#5] WorkspaceDefaultVC 화면 수정

--- Sparkle/Presentation/WorkSpace/Home/ViewController/HomeDefaultViewController.swift
+++ Sparkle/Presentation/WorkSpace/Home/ViewController/HomeDefaultViewController.swift
@@ -13,8 +13,8 @@ import RxCocoa
 
 final class HomeDefaultViewController: BaseViewController<HomeDefaultView> {
     
-    private let reactor = HomeDefaultViewReactor()
-    private let disposeBag = DisposeBag()
+     
+    var disposeBag = DisposeBag()
     private var workspaceId: String?
     
     init(workspaceId: String?) {
@@ -28,69 +28,40 @@ final class HomeDefaultViewController: BaseViewController<HomeDefaultView> {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        bind(reactor: reactor)
-        if let workspaceId {
-            reactor.action.onNext(.fetchChannelData(workspaceID: workspaceId))
-        }
+        self.reactor = HomeDefaultViewReactor()
     }
     
     override func setupUI() {
-//        rootView.channelTableView.delegate = nil
-//        rootView.channelTableView.dataSource = nil
-        print("====setupUI")
         rootView.channelTableView.rowHeight = UITableView.automaticDimension
         rootView.channelTableView.estimatedRowHeight = 44
-        rootView.channelTableView.separatorStyle = .none // 선택사항
-        
-        rootView.channelTableView.register(ChannelsTableViewCell.self, forCellReuseIdentifier: ChannelsTableViewCell.identifier)
-        
-//        rootView.directTableView.delegate = self
-//        rootView.directTableView.dataSource = self
-        rootView.directTableView.register(UserTableViewCell.self, forCellReuseIdentifier: UserTableViewCell.identifier)
-    }
-    
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWill")
-//        channelNetworkRequest()
+        rootView.channelTableView.separatorStyle = .none
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        // 테이블뷰 높이 업데이트
-//        rootView.updateChannelTableViewHeight()
+        
         rootView.updateDirectTableViewHeight()
     }
+}
+
+
+extension HomeDefaultViewController: View {
     
-//    private func channelNetworkRequest() {
-//        if let workspaceId {
-//            
-//            ChannelsNetworkManager.shared.myChannelCheck(parameters: WorkspaceIDParameter(workspaceID: workspaceId))
-//                .subscribe(with: self) { owner, response in
-//                    print(response)
-//                } onFailure: { owner, error in
-//                    print("에러 입니다!~~~!\(error)")
-//                }
-//                .disposed(by: disposeBag)
-//        }
-//    }
-    
-    private func dmsNetworkRequest() {
-        
-    }
-    
-    private func bind(reactor: HomeDefaultViewReactor) {
+     func bind(reactor: HomeDefaultViewReactor) {
+         
+         Observable.just(workspaceId)
+             .compactMap { $0 }
+             .map { HomeDefaultViewReactor.Action.fetchChannelData(workspaceID: $0) }
+             .bind(to: reactor.action)
+             .disposed(by: disposeBag)
+         
         rootView.addChannelButton.rx.tap
             .map { HomeDefaultViewReactor.Action.addChannelsButtonTapped }
             .bind(to: reactor.action)
             .disposed(by: disposeBag)
         
         reactor.state.map { $0.channelData }
-//            .distinctUntilChanged()
             .bind(with: self) { owner, _ in
-                
                 owner.rootView.updateChannelTableViewHeight()
             }
             .disposed(by: disposeBag)
@@ -103,9 +74,6 @@ final class HomeDefaultViewController: BaseViewController<HomeDefaultView> {
             }
             .disposed(by: disposeBag)
         
-        let testData = [ChannelResponse(channel_id: "d083709a-0885-4179-9878-706e65f50e1b", name: "일반", description: "", coverImage: "ㅇ", owner_id: "b0365afe-a99d-4d3b-ab7d-4897c3aed288", createdAt: "create")]
-
-       
         
         reactor.state
             .map({ $0.channelData })
@@ -124,33 +92,4 @@ final class HomeDefaultViewController: BaseViewController<HomeDefaultView> {
     }
 }
 
-//extension HomeDefaultViewController: UITableViewDelegate, UITableViewDataSource {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        if tableView == rootView.channelTableView {
-//            return 5
-//        } else {
-//            return 2
-//        }
-//    }
-//    
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if tableView == rootView.channelTableView {
-//            guard let cell = tableView.dequeueReusableCell(withIdentifier: ChannelsTableViewCell.identifier, for: indexPath) as? ChannelsTableViewCell else { return UITableViewCell() }
-//            return cell
-//        } else {
-//            guard let cell = tableView.dequeueReusableCell(withIdentifier: UserTableViewCell.identifier, for: indexPath) as? UserTableViewCell else { return UITableViewCell() }
-//            return cell
-//        }
-//    }
-//    
-//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        return 41
-//    }
-//    
-//    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-//   
-//        rootView.updateChannelTableViewHeight()
-//        rootView.updateDirectTableViewHeight()
-//        
-//    }
-//}
+
